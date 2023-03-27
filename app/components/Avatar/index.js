9e02769db38a29bf44106000eeb0681fca68322c
dhruv-growexxer <dhruv.chauhan@growexx.com>
2023-03-27 12:07:39 +0530

    fix: 🐛 Header dropdown fixed
    Still 3 test cases are breaking will check after updating to major patch

--- app/components/Avatar/index.js
+++ app/components/Avatar/index.js
@@ -5,28 +5,29 @@
  * This is the Avatar Component file.
  */
 import React from 'react';
-import { Dropdown, Menu } from 'antd';
+import { Dropdown, Button } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { AvatarWrapper } from './StyledAvatar';
 
-const getMenu = MenuItems => (
-  <Menu>
-    {MenuItems.map((menuItem, index) => (
-      <Menu.Item key={index} icon={menuItem.icon}>
-        <Link to={menuItem.to}>{menuItem.tabName}</Link>
-      </Menu.Item>
-    ))}
-  </Menu>
-);
+const returnItems = itemArr => ({
+  items: getNewMenu(itemArr),
+});
+
+const getNewMenu = MenuItems =>
+  MenuItems.map((menuItem, index) => ({
+    key: index,
+    label: <Link to={menuItem.to}>{menuItem.tabName}</Link>,
+  }));
+
 const Avatar = props => (
   <AvatarWrapper>
-    <Dropdown.Button
-      menu={getMenu(props.menu)}
-      placement="bottom"
-      icon={<UserOutlined />}
-    />
+    <Dropdown menu={returnItems(props.menu)} placement="bottom">
+      <Button type="primary" shape="circle">
+        <UserOutlined />
+      </Button>
+    </Dropdown>
   </AvatarWrapper>
 );
 
