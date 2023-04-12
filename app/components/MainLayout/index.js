e5d56184fd87cbcd828ec79fe5ec054eb1a25fb0
dhruv-growexxer <dhruv.chauhan@growexx.com>
2023-04-12 17:36:54 +0530

    fix: 🐛 login logout fixed

--- app/components/MainLayout/index.js
+++ app/components/MainLayout/index.js
@@ -1,13 +1,8 @@
-/**
- * MainLayout.js
- *
- * This is the entry file for the application, only setup and boilerplate
- * code.
- */
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Spin } from 'antd';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import { makeSelectAppLoading } from 'containers/App/selectors';
 import App from 'containers/App';
@@ -18,70 +13,73 @@ import { LAYOUT_CONFIG } from '../constants';
 import { StyledMainLayout } from './StyledMainLayout';
 import Layouts from './Layout';
 
-function MainLayout(props) {
-  const { appLoading, defaultLayout, location } = props;
-  const [collapsed, setCollapsed] = useState(true);
-  const [layoutVariant, setLayoutVariant] = useState(defaultLayout);
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(location);
-    const layoutVariantFromUrl = urlParams.get('layout')
+class MainLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    const urlParams = new URLSearchParams(props.location.search);
+    const layoutVariant = urlParams.get('layout')
       ? +urlParams.get('layout')
-      : defaultLayout;
-    setLayoutVariant(layoutVariantFromUrl);
-    setCollapsed(
-      ![LAYOUT_CONFIG.VERTICAL_OPTION_2].includes(layoutVariantFromUrl),
-    );
-  }, [defaultLayout, location]);
-
-  useEffect(() => {
-    function handleLogIn() {
-      forceUpdate();
-    }
-    function handleLogOut() {
-      forceUpdate();
-    }
-
-    Emitter.on(EMITTER_EVENTS.LOG_IN, handleLogIn);
-    Emitter.on(EMITTER_EVENTS.LOG_OUT, handleLogOut);
-
-    return () => {
-      Emitter.off(EMITTER_EVENTS.LOG_IN, handleLogIn);
-      Emitter.off(EMITTER_EVENTS.LOG_OUT, handleLogOut);
+      : props.defaultLayout;
+    this.state = {
+      collapsed: ![LAYOUT_CONFIG.VERTICAL_OPTION_2].includes(layoutVariant),
+      layoutVariant,
     };
-  }, []);
-
-  function forceUpdate() {
-    // This is to force a re-render of the component
-    setCollapsed(collapsed);
   }
 
-  if (userExists()) {
-    return (
-      <Spin spinning={appLoading} size="default">
-        <StyledMainLayout
-          data-environment={
-            process.env.NODE_ENV !== 'production' ? process.env.NODE_ENV : null
-          }
-          className="main-layout"
-        >
-          <Layouts
-            collapsed={collapsed}
-            layoutVariant={layoutVariant}
-            toggle={() => setCollapsed(!collapsed)}
-          />
-        </StyledMainLayout>
-      </Spin>
-    );
+  toggle = () => {
+    const { collapsed } = this.state;
+    this.setState({
+      collapsed: !collapsed,
+    });
+  };
+
+  componentDidMount() {
+    Emitter.on(EMITTER_EVENTS.LOG_IN, () => {
+      this.setState({});
+    });
+    Emitter.on(EMITTER_EVENTS.LOG_OUT, () => {
+      this.setState({});
+    });
   }
 
-  return <App />;
+  componentWillUnmount() {
+    Emitter.off(EMITTER_EVENTS.LOG_IN);
+    Emitter.off(EMITTER_EVENTS.LOG_OUT);
+  }
+
+  render() {
+    const { appLoading } = this.props;
+    const { layoutVariant, collapsed } = this.state;
+
+    if (userExists()) {
+      return (
+        <Spin spinning={appLoading} size="default">
+          <StyledMainLayout
+            data-environment={
+              process.env.NODE_ENV !== 'production'
+                ? process.env.NODE_ENV
+                : null
+            }
+            className="main-layout"
+          >
+            <Layouts
+              collapsed={collapsed}
+              layoutVariant={layoutVariant}
+              toggle={this.toggle}
+            />
+          </StyledMainLayout>
+        </Spin>
+      );
+    }
+
+    return <App />;
+  }
 }
 
 MainLayout.propTypes = {
   appLoading: PropTypes.bool,
-  location: PropTypes.object,
   defaultLayout: PropTypes.number,
+  location: PropTypes.string,
 };
 
 MainLayout.defaultProps = {
@@ -92,4 +90,14 @@ const mapStateToProps = createStructuredSelector({
   appLoading: makeSelectAppLoading(),
 });
 
-export default connect(mapStateToProps)(MainLayout);
+const withConnect = connect(mapStateToProps);
+
+const MainLayoutWithConnect = withConnect(MainLayout);
+
+const MainLayoutWrapper = () => {
+  const location = useLocation();
+
+  return <MainLayoutWithConnect location={location} />;
+};
+
+export default MainLayoutWrapper;
