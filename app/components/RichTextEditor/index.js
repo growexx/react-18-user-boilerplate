cd390bc74e9fc1ba70b3f8b92b7dcd2c72c3845b
jainil-growexxer <jainil.patel@growexx.com>
2023-08-31 15:51:46 +0530

    fix: removing code smell

--- app/components/RichTextEditor/index.js
+++ app/components/RichTextEditor/index.js
@@ -10,17 +10,20 @@ import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 import { StyledEditor } from './StyledEditor';
+import { StyledDraft } from './StyledDraft';
 
 function RichTextEditor({ value, onChange }) {
   const editorRef = useRef(null);
 
   return (
     <StyledEditor>
-      <Editor
-        ref={editorRef}
-        editorState={value}
-        onEditorStateChange={onChange}
-      />
+      <StyledDraft>
+        <Editor
+          ref={editorRef}
+          editorState={value}
+          onEditorStateChange={onChange}
+        />
+      </StyledDraft>
     </StyledEditor>
   );
 }
