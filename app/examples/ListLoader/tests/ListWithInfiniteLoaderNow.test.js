fc9e14149fbf22c08c12fc755307bd634ec319c5
brijesh-growexxer <brijesh.thakkar@growexx.com>
2023-09-04 15:05:57 +0530

    test: 💍 list with infinite loader

--- app/examples/ListLoader/tests/ListWithInfiniteLoaderNow.test.js
+++ app/examples/ListLoader/tests/ListWithInfiniteLoaderNow.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
@@ -7,8 +8,24 @@ import history from 'utils/history';
 import request from 'utils/request';
 import { store } from 'configureStore';
 import ListWithInfiniteLoader from '../ListWithInfiniteLoader';
+import { responseWithLargeDataSet } from '../stub/test.stub';
+
 let globalStore;
 jest.mock('utils/request');
+jest.mock('react-infinite-scroller', () => {
+  function LoadMore({ children, loadMore }) {
+    return (
+      <div data-testid="infinite-scroll">
+        {children}
+        <button onClick={loadMore} data-testid="load-more-button" type="submit">
+          Load More
+        </button>
+      </div>
+    );
+  }
+  return LoadMore;
+});
+
 const componentWrapper = () =>
   render(
     <Provider store={globalStore}>
@@ -19,6 +36,7 @@ const componentWrapper = () =>
       </IntlProvider>
     </Provider>,
   );
+
 describe('<ListWithInfiniteLoader />', () => {
   beforeAll(() => {
     globalStore = store;
@@ -50,5 +68,14 @@ describe('<ListWithInfiniteLoader />', () => {
     } = componentWrapper();
     await waitFor(() => expect(request).toHaveBeenCalledTimes(2));
     expect(firstChild).toMatchSnapshot();
+  });
+
+  it('should load more items when scrolling', async () => {
+    request.mockImplementation(() => Promise.resolve(responseWithLargeDataSet));
+    const { getByTestId } = componentWrapper();
+    waitFor(() => {
+      getByTestId('load-more-button').click();
+    });
+    await waitFor(() => expect(request).toHaveBeenCalledTimes(4));
   });
 });
